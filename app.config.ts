2aa35a1f2298c14891dab262e9640365a8a4ca60
venvadlamani <122305594+venvadlamani@users.noreply.github.com>
2023-05-21 07:00:04 -0500

    1 file modified

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,14 @@
 export default defineAppConfig({
   ui: {
-    primary: 'green',
+    primary: 'emerald',
     gray: 'cool',
+    notifications: {
+      position: 'top-0 right-0',
+    },
+    card: {
+      background: 'bg-gray-200 dark:bg-gray-900',
+      ring: 'ring-0',
+      rounded: 'rounded-2xl',
+    },
   },
 });
