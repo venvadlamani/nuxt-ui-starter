52ab0a977a1d87f71ff18596f97ad5ad55695bdc
venvadlamani <122305594+venvadlamani@users.noreply.github.com>
2023-05-20 20:04:45 -0500

    1 file modified

--- app.vue
+++ app.vue
@@ -1,9 +1,8 @@
 <template>
-  <div class="container bg-gray-100 text-gray-900 min-h-screen">
-    <h1>hello world</h1>
-  </div>
+    <div class="container bg-gray-100 text-gray-900 min-h-screen">
+        <h1>hello world</h1>
+    </div>
 </template>
 <script setup>
 const user = useSupabaseUser();
-
 </script>
